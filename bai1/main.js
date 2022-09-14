26124d33acb08b9b361b164f9d39f69c80920117
PQHtobi <phanquochung2511@gmail.com>
2022-09-14 20:32:15 +0700

    .

--- bai1/main.js
+++ bai1/main.js
@@ -53,4 +53,103 @@ let [ten, email = 'phanquochung2511@gmail.com'] = name;
 console.log(ten);
 console.log(email);
 // bài 4 default parameters in ES6
- 
+// cách khái báo biến mặc định ES6
+var hello = (domain) => {
+    domain = domain || 'PQH tobi';
+    return domain;
+}
+console.log('khong truyền tham số: ' + hello());
+console.log('có truyền tham số: ' + hello('Phan Quốc Hưng tobi'));
+// khai báo mặc định trong ES6
+let helloes6 = (domain1 = 'PQH tobi') =>{
+    return domain1;
+}
+console.log('khong truyền tham số es6: ' + helloes6());
+console.log('có truyền tham số es6: ' + helloes6('Phan Quốc Hưng tobi'));
+
+// bài 5 rest parameters in ES6
+//khai báo một hàm với số lượng thạm số không xác định
+let functionName = (param1, param2, ...other) =>{
+}
+let domainList = (main,sub, ...other) =>{
+    console.log('Main: '+main);
+    console.log('Sub: '+sub);
+    console.log('Other');
+    console.log(other);
+}
+domainList('freetuts.net', 'facebook.com', 'google.com', 'zalo.com', 'iphone.com');
+
+//bai 6 block scoped 
+// phạm vi trong một khối, nghĩa là chỉ hoạt động trong phạm vi được khai báo bới dấu cặp {}
+// {
+//     var a = 6;
+//     var a = 7;
+//     console.log(a);
+// }
+{
+    var a = 6;
+    var printA = () =>{
+        var a = 7;
+        console.log(a);
+    }
+    printA();
+    console.log(a);
+}
+
+// template literals in ES6
+//hiển thị các biến trong chuỗi.
+var template = (first,last) =>{
+    let name = `your name is ${first} ${last}`;
+    console.log(name);
+}
+template('hung','tobi');
+
+//multi-line string in es6
+// xuống dòng văn bản trong js
+//es5
+var roadPoemes5 = 'test\n\t'
++'multi-line\n\t'
++'Es5 \n\t'
+console.log(roadPoemes5);
+//es6 
+var roadPoem = `PQH
+Tobi
+Đẹp
+Trai`
+console.log(roadPoem);
+
+//destructuring assignment in es6
+
+//enhanced object literals in es6
+
+//arrow function in es6
+// $('.btn').click((event) =>{
+//     this.sendData()
+//   })
+
+//   var wait1000 =  new Promise((resolve, reject)=> {
+//     setTimeout(resolve, 4000)
+//   }).then(()=> {
+//     console.log('Yay!')
+//   })
+
+// var wait1000 =  ()=> new Promise((resolve, reject)=> {setTimeout(resolve, 1000)})
+
+// wait1000()
+//     .then( () => {
+//         console.log('Yay!')
+//         return wait1000()
+//     })
+//     .then( () => {
+//         console.log('Wheeyee!')
+//     });
+// let dùng để khai báo biến trong phạm vi nhưng let là biến có thể thay đổi 
+// vd: let name ='said';
+//         name = 'rick';
+// const thì không thể thay đổi và không thể gán lại được nữa 
+// let và const đều giới hạn trong phạm vi được khai báo {}
+
+ const myArray = ['PQH','Tobi','Aya','Hayate',5];
+ let Arr2 = myArray.map(item => item);
+
+ console.log(Arr2);
